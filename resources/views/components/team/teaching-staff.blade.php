799903065c873397005033151cabad1a9669db61
Shruti-Orbit <scdhr21@gmail.com>
2026-01-08 11:29:49 +0530

    Update teaching staff designation to include "New Era" for clarity

--- resources/views/components/team/teaching-staff.blade.php
+++ resources/views/components/team/teaching-staff.blade.php
@@ -67,7 +67,7 @@
                 </h3>
 
                 <p class="text-sm text-gray-500 mb-2">
-                    English Teacher </p>
+                    English Teacher New Era </p>
 
                 <div class="flex justify-center gap-3 text-accent">
                     <i class="fa-solid fa-book-open"></i>
